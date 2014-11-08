a3c5459f2026e89fdb0793ec73864d3c4eff8e66
Will Huang <doggy.huang@gmail.com>
2014-11-08 22:00:02 +0800

    加入 Visual Studio 2013 Update 3 內建的 ASP.NET MVC 5 專案範本
    [後端套件]
    * ASP.NET MVC 5.2.0
      - 官網: http://www.asp.net/mvc
      - 專案位址: http://aspnetwebstack.codeplex.com/
    * ASP.NET Web API 5.2.0
      - 官網: http://www.asp.net/web-api
      - 專案位址: http://aspnetwebstack.codeplex.com/
    * ASP.NET Identity 2.1.0
      - 官網: http://www.asp.net/identity
      - 專案位址: https://aspnetidentity.codeplex.com/
    * Entity Framework 6.1.1
      - 官網: http://www.asp.net/entity-framework
      - EF6 專案: https://entityframework.codeplex.com/
      - EF7 專案: https://github.com/aspnet/EntityFramework
    * WebGrease 1.5.2
      - WebGrease is a suite of tools for optimizing javascript, css files and images.
      - 專案位址: https://webgrease.codeplex.com/
    * Microsoft.AspNet.Web.Optimization 1.1.3
      - CSS, JS 最小化與打包工具 (Bundling)
      - ASP.NET Optimization introduces a way to bundle and optimize CSS and JavaScript files.
    * OWIN 1.0
      - 官網: http://owin.org/
    * Microsoft.Owin 2.1 (Katana)
      - 專案位址: http://katanaproject.codeplex.com/
    
    [前端套件]
    * jQuery 1.10.2
    * jQuery Validation 1.11.1
    * Microsoft.jQuery.Unobtrusive.Validation 3.2.0
    * Bootstrap 3.0.0
    * Modernizr 2.6.2
    * Respond 1.2.0
      - RWD 的 Polyfills

---
+++ Mvc5Day1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc5Day1.MvcApplication" Language="C#" %>
---
+++ Mvc5Day1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5Day1.Startup))]
+namespace Mvc5Day1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
